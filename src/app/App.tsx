ccf5fbe24ea4ed934eb6c94d487f4f9b3c6d562a
denis kusherekin <denkus.denikus@gmail.com>
2025-03-23 12:09:55 +0200

    feat(sidebar.tsx): add sidebar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+// src/App.tsx
+import { useState } from 'react';
+import reactLogo from '@/assets/react.svg';
+import viteLogo from '/vite.svg';
+import Sidebar from '../shared/components/Sidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" className='h-100'>
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} className='bg-firefly-2 text-white w-fit m-4 p-4 rounded-xl cursor-pointer hover:opacity-70 transition-all duration-300'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Sidebar />
+      <main className="flex-grow">
+        <section className="text-center mt-10">
+          <div>
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo mx-auto" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react mx-auto" alt="React logo" />
+            </a>
+          </div>
+          <h1 className="text-xl mt-5">Vite + React</h1>
+          <div className="card mx-auto mt-5">
+            <button onClick={() => setCount(count => count + 1)} className='bg-firefly-2 text-white w-fit mx-4 my-4 p-4 rounded-xl cursor-pointer hover:opacity-70 transition-all duration-300'>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR updates
+            </p>
+          </div>
+          <p className="mt-5">
+            Click on the Vite and React logos to learn more
+          </p>
+        </section>
+      </main>
+      <footer className="bg-gray-800 text-white text-center p-4">
+        © {new Date().getFullYear()} Digital Dictionary
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
